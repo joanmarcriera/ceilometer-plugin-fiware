b8c1c0831cf3876363c832b02169407f8a2fe821
Daniele Santoro <psyacca@gmail.com>
2016-04-14 10:07:13 +0200

    compute_pollster/memory.py: Change to correct version 1.0.0

--- compute_pollster/memory.py
+++ compute_pollster/memory.py
@@ -3,7 +3,7 @@
 #
 # Author: Attilio Broglio <abroglio AT create-net DOT org>
 #
-# Version: 1.1.0
+# Version: 1.0.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
